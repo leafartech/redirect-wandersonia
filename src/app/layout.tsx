35cb5ebe9745609f5549d83dacd52ece48f82f6c
Rafael Borges Bezerra <rafaelborgesbezerra@Rafaels-MacBook-Pro.local>
2024-11-15 10:11:05 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Redirect - Teateen",
+  description: "Link de redirect oficial Teateen",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N7P74942')
+        `}} />
+      </head>
+      <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7P74942"
+          height="0" width="0"></iframe></noscript>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const [steps, setSteps] = useState(0)
+  let time = 0
+  const router = useRouter()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      time++;
+      if (time >= 10) {
+        clearInterval(interval);
+      }
+    }, 500);
+
+    const animationInterval = setInterval(() => {
+      setSteps((prevState) => {
+        if (prevState < 100) {
+          return prevState + 1;
+        }
+        clearInterval(animationInterval);
+        return prevState;
+      });
+    }, 50);
+
+    return () => {
+      clearInterval(interval);
+      clearInterval(animationInterval);
+    };
+  }, []);
+
+  const barStyle = {
+    width: `${steps}%`,
+    transition: "width 0.5s ease-in-out" // Adicionando a animação ease-in-out
+  };
+
+  if (barStyle.width === '100%') {
+    setTimeout(() => {
+      // router.push('https://chat.whatsapp.com/DAnKidM9wpqLV65rLdfqa1')
+    }, 500)
+  }
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center bg-white">
+      <div className="relative flex flex-col text-center items-center justify-center sm:h-[444px] px-4 sm:px-12 rounded-md">
+        <img
+          src="./images/logo.png"
+          alt=""
+          className="-translate-y-6 w-[54%] w-full sm:w-64 sm:h-[7em] object-contain " />
+        <div>
+          <h2 className="text-3xl font-semibold">Aguarde um momento,</h2>
+          <p className="text-zinc-700 leading-5 sm:text-base text-sm">Estamos procurando o melhor grupo para você...</p>
+        </div>
+        <div className="relative flex justify-center items-center rounded-full w-full h-8 mt-4 bg-gray-200 overflow-hidden">
+          <div className={`absolute left-0 h-full bg-[#0676ff]`} style={barStyle}></div>
+          {barStyle.width === '100%' && (
+            <svg viewBox="25 25 50 50" className="z-10 absolute">
+              <circle r="20" cy="50" cx="50"></circle>
+            </svg>
+          )}
+        </div>
+      </div>
+    </main>
+  )
+}
