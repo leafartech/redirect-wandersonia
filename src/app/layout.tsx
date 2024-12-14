21374913825db89656668675110d66dfa8580766
Rafael Borges Bezerra <leafartech@gmail.com>
2024-12-14 09:41:22 -0300

    page name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Redirect - Teateen",
-  description: "Link de redirect oficial Teateen",
+  title: "Redirect - Wandersonia",
+  description: "Link de redirect oficial Wandersonia",
 };
 
 export default function RootLayout({
